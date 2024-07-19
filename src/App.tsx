5fd938f65547c51a5ec68464a81d69d1a30f2c92
Miroslau <forse2408@gmail.com>
2024-07-19 20:03:30 +0300

    feat: add modal for display double windows
    Signed-off-by: Miroslau <forse2408@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,119 +1,65 @@
+import Game from './components/game/game.tsx';
 import { useEffect, useState } from 'react';
-import Header from './components/header/header.tsx';
-import GameBoard from './components/game-board/game-board.tsx';
-import gameLevels from '../src/constants/levels/levels.json';
-import LetterController from './components/letter-controller/letter-controller.tsx';
-import getMinimalLettersSet from './utils/getMinimalLettersSet.ts';
-import { LetterType } from './type/LetterType.ts';
-import BoardType from './type/BoardType.ts';
-import { winValidation } from './utils/winValidation.ts';
-import { LevelType } from './type/LevelType.ts';
+import { v4 as uuidv4 } from 'uuid';
+import Modal from './components/portal/modal.tsx';
 
 function App() {
-  const [currentlevel, setCurrentLevel] = useState<number>(1);
-  const [countLevel, setCount] = useState<number>(1);
-  const [board, setBoard] = useState<BoardType[]>([]);
-  const [letters, setLetters] = useState<LetterType[]>([]);
-  const [selectedLetters, setSelectedLetters] = useState<string[]>([]);
+  const [isActive, setIsActive] = useState<boolean>(true);
+  const [sessionId, _] = useState<string>(uuidv4());
 
-  const winner = winValidation(board);
-
-  const level: LevelType = gameLevels.levels.find(
-    (level) => level.id === currentlevel
-  ) as LevelType;
-
-  const checkWord = (word: string) => {
-    const foundedWord = board.find((row) => row.value === word);
-    const foundedWordIndex = board.findIndex((row) => row.value === word);
-    if (foundedWord && !foundedWord.founded) {
-      console.log('we found word: ', word);
-      const wordLetters = word.split('');
-      const updateBoard: BoardType = {
-        ...board[foundedWordIndex],
-        founded: true,
-        letters: [...wordLetters]
-      };
-
-      const newBoard = [
-        ...board.slice(0, foundedWordIndex),
-        updateBoard,
-        ...board.slice(foundedWordIndex + 1)
-      ];
-
-      setBoard(newBoard);
-    }
-  };
-
-  const changeLevel = () => {
-    setCurrentLevel((prevState) => (prevState + 1 > 3 ? 1 : prevState + 1));
-    setCount((prevState) => prevState + 1);
+  const handleRefresh = () => {
+    setIsActive(true);
+    localStorage.setItem('activeSession', sessionId);
+    window.location.reload();
   };
 
   useEffect(() => {
-    if (currentlevel > 3) {
-      setCurrentLevel(1);
-    } else {
-      const letters = getMinimalLettersSet(level.words);
-      setLetters(
-        letters.map((letter, index) => ({
-          id: index + 1,
-          value: letter
-        }))
-      );
-      setBoard(
-        level.words
-          .sort((a, b) => a.length - b.length)
-          .map((word) => ({
-            value: word,
-            letters: Array(word.length).fill(null),
-            founded: false
-          }))
-      );
-    }
-  }, [currentlevel]);
+    localStorage.setItem('activeSession', sessionId);
 
-  useEffect(() => {
-    console.log(board);
-  }, [board]);
+    const handleStorageChange = (event: StorageEvent) => {
+      if (event.key === 'activeSession' && event.newValue !== sessionId) {
+        setIsActive(false);
+      }
+    };
 
-  if (winner) {
-    return (
-      <main className="w-screen h-screen bg-color-pickled">
-        <div className="py-44 px-40 flex flex-col justify-between items-center gap-y-28">
-          <div>
-            <div className="text-white text-[37px] font-bold text-center">
-              Уровень {countLevel} пройден
-            </div>
-            <div className="text-white text-[37px] font-bold text-center">Изумительно!</div>
-          </div>
-          <button
-            onClick={changeLevel}
-            className="max-w-[130px] w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-            Уровень {countLevel + 1}
-          </button>
-        </div>
-      </main>
-    );
-  }
+    const handleVisibilityChange = () => {
+      if (document.visibilityState === 'visible') {
+        localStorage.setItem('activeSession', sessionId);
+        setIsActive(true);
+      }
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
+    };
+  }, [sessionId]);
 
   return (
-    <main className="w-screen h-screen bg-color-pickled">
-      <div className="p-7">
-        <Header
-          headerStyle="flex justify-center items-center gap-3"
-          textStyle="text-white text-[30px] font-[700]"
-          title="Уровень"
-          gameLevel={countLevel}
-        />
-        <GameBoard board={board} level={level} />
-        <LetterController
-          letters={letters}
-          selectedLetters={selectedLetters}
-          setSelectedLetters={setSelectedLetters}
-          onWordSelected={checkWord}
-        />
-      </div>
-    </main>
+    <div>
+      {!isActive && (
+        <Modal>
+          <div className="text-center w-60">
+            <div className="mx-auto my-4 w-52">
+              <h3 className="text-lg font-black">Две вкладки с игрой?</h3>
+              <p className="text-sm text-gray-500">
+                Похоже, игра открыта в нескольких вкладках браузера. Чтобы продолжить играть в этой
+                вкладке, обновите страницу.
+              </p>
+            </div>
+            <button
+              onClick={handleRefresh}
+              className="bg-green-500 hover:bg-green-700 active:bg-green-800 px-4 py-2 rounded-md text-white">
+              Обновить
+            </button>
+          </div>
+        </Modal>
+      )}
+      <Game />
+    </div>
   );
 }
 
--- src/components/portal/modal.tsx
+++ src/components/portal/modal.tsx
@@ -6,8 +6,10 @@ interface ModalProps extends PropsWithChildren {}
 const Modal: FC<ModalProps> = ({ children }) => {
   return (
     <Portal wrapperId="react-portal-modal-container">
-      <div className="fixed inset-0 bg-black bg-opacity-60 flex flex-col items-center justify-center transition-all duration-300 ease-in-out overflow-hidden z-[999] p-10 pt-20">
-        <div className="w-[70%] h-[70%] bg-white rounded-lg text-white flex-col items-center justify-center text-2xl">
+      <div className="fixed inset-0 flex justify-center items-center transition-colors bg-black/20">
+        <div
+          onClick={(e) => e.stopPropagation()}
+          className="bg-white rounded-xl shadow p-6 transition-all">
           {children}
         </div>
       </div>
